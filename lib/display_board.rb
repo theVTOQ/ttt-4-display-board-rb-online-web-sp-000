00b2beea8ef1571a12a9c41703ab3b2a940bf915
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-30 00:07:53 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,26 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  row1 = formatRow(board, 0)
+  row2 = formatRow(board, 3)
+  row3 = formatRow(board, 6)
+  divider = "-----------"
+
+
+  puts row1
+  puts divider
+  puts row2
+  puts divider
+  puts row3
+end
+
+def formatRow(board, firstIndex)
+  row = formatCell(board[firstIndex])
+  row << "|" + formatCell(board[firstIndex + 1])
+  row << "|" + formatCell(board[firstIndex + 2])
+  row
+end
+
+def formatCell(entry)
+  " #{entry} "
+end
